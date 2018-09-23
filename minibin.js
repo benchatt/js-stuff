c8088a4501b15a13a5e79a6cece21187f22c2265
Ben Chatterton <ben.chatterton@gmail.com>
2018-09-22 17:00:59 -0700

    mini-binary app

--- minibin.js
+++ minibin.js
@@ -3,15 +3,15 @@ function doToggle(shape) {
     var delta = 0;
     if (xa.active===true) {
         //deactivating
-        shape.attr({fill:'#ecc'});
-        xa.undertext.attr({fill:'#aaa',text:"0"});
+        shape.attr({fill:"#ecc"});
+        xa.undertext.attr({fill:"#aaa",text:"0"});
         xa.ontext.attr({text:"0"});
         xa.active=false;
         delta = delta - xa.mult;
     } else if (xa.active===false) {
         //activating
-        shape.attr({fill:'#cec'});
-        xa.undertext.attr({fill:'#222',text:String(xa.mult)});
+        shape.attr({fill:"#cec"});
+        xa.undertext.attr({fill:"#222",text:String(xa.mult)});
         xa.ontext.attr({text:"1"});
         xa.active=true;
         delta = delta + xa.mult;
@@ -28,11 +28,11 @@ window.onload = function () {
     var rounding = parseInt(PAD / 2);
     var total = 0;
     var sum = paper.text(440,60,"=0")
-        .attr({fill:'#222', 'font-family':'monospace', 'font-size':60});
+        .attr({fill:"#222", 'font-family':'monospace', 'font-size':60});
     for (var i=0;i<size-1;i++) {
         var xoffset = (i * (SIDE + PAD)) + PAD;
         paper.text(xoffset+SIDE,PAD+(SIDE*1.5),"+")
-            .attr({fill:'#222', 'font-family':'monospace', 'font-size':20});
+            .attr({fill:"#222", 'font-family':'monospace', 'font-size':20});
     }
     var spaces = (function (paper){
         var spaces = [];
@@ -40,11 +40,11 @@ window.onload = function () {
             var xoffset = (i * (SIDE + PAD)) + PAD;
             var mult = Math.pow(2,3-i);
             var shape = paper.rect(xoffset,PAD,SIDE,SIDE,rounding)
-                            .attr({fill:'#ecc', stroke:'#200', 'stroke-width':3});
+                            .attr({fill:"#ecc", stroke:"#200", 'stroke-width':3});
             var undertext = paper.text(xoffset+(SIDE*0.5),PAD+(SIDE*1.5),"0")
-                            .attr({fill:'#aaa', 'font-family':'monospace', 'font-size':20});
+                            .attr({fill:"#aaa", 'font-family':'monospace', 'font-size':20});
             var ontext = paper.text(xoffset+(SIDE*0.5),PAD+(SIDE/2),"0")
-                            .attr({fill:'#222', 'font-family':'monospace', 'font-size':40})
+                            .attr({fill:"#222", 'font-family':'monospace', 'font-size':40})
                             .data("shape",shape);
             var xattrs = {
                 active:false,
